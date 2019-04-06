aa27bc8ad1a8932a3d23c024be89601bc8be72a2
Pedram Kasebzadeh <pedka102@student.liu.se>
2019-04-06 20:58:54 +0200

    KKNN

--- Logistic Regression .R
+++ Logistic Regression .R
@@ -24,4 +24,23 @@ Missclassification = (tab[1,2]+tab[2,1])/sum(tab)
 # Misclassification rate confucsion Matrix based on Y=1 if Predected Y>0.9,else = 0 
 pred2 <- ifelse(prediction>0.9,1,0)
 tab2 <- table(Prediction=pred2,Actual=train$Spam)
-misclassification = (tab2[1,2]+tab2[2,1])/sum(tab)
+misclassification = (tab2[1,2]+tab2[2,1])/sum(tab2)
+
+
+# misclasification resulted from KKNN 
+library(kknn)
+kknn <- kknn(Spam~.,train=train,test=test,k=30)
+summary(kknn)
+
+# Misclassification rate confucsion Matrix based on Y=1 if Predected Y>0.5,else = 0 
+predictkn <- ifelse(kknn$fitted.values>0.5,1,0)
+tab3 <- table(Predicted=predictkn,Actual=test$Spam)
+kknnmisclassification = (tab3[1,2]+tab3[2,1])/sum(tab3)
+
+# Misclassification rate confucsion Matrix based on Y=1 if Predected Y>0.9,else = 0 
+
+predictknn <- ifelse(kknn$fitted.values>0.9,1,0)
+tab4 <- table(Predicted=predictknn,Actual=test$Spam)
+kknnmisclassification0.9 = (tab4[1,2]+tab4[2,1])/sum(tab4)
+
+
